d2414c9032cdbf083f253a36836d8c37215e4d75
Mattia Solimeno <mattia.solimeno15@gmail.com>
2025-11-24 18:12:21 +0100

    + music, sfx and character movement animation

--- characters/Character.gd
+++ characters/Character.gd
@@ -14,6 +14,8 @@ enum ClickType {
 @onready var extraSlot: Sprite3D = %ExtraSprite
 
 @onready var comicsSprite: Sprite3D = %ComicsSprite
+@onready var correct_audio_player: AudioStreamPlayer = $CorrectAudioPlayer
+@onready var incorrect_audio_player: AudioStreamPlayer = $IncorrectAudioPlayer
 
 @export var comicsOk: Texture2D
 @export var comicsError: Texture2D
@@ -99,11 +101,13 @@ func check_is_target() -> void:
 	
 	if is_target:
 		comicsSprite.texture = comicsOk
+		correct_audio_player.play()
 		print("✓ Target corretto!")
 		print("++++++++++++++++++++++")
 
 	else:
 		comicsSprite.texture = comicsError
+		incorrect_audio_player.play()
 		speed = max_speed
 		print("✗ Target sbagliato!")
 		print("-------------------------")
